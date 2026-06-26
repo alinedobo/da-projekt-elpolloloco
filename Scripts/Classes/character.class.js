2beae7e70014189631fe8e364823f9ed8ea606f0
Aline Dobo <dobo.aline@gmail.com>
2026-06-26 09:23:38 +0200

    full background and resized charcters

--- Scripts/Classes/character.class.js
+++ Scripts/Classes/character.class.js
@@ -6,7 +6,7 @@ export class Character extends MovableObject{
     constructor(){
         super().loadImage('../Assets/img/2_character_pepe/2_walk/W-21.png');
         this.position_x = 20;
-        this.position_y = 200;
+        this.position_y = 235;
         this.height = 200;
         this.width = 100;
     }
--- Scripts/Classes/enemy.class.js
+++ Scripts/Classes/enemy.class.js
@@ -5,8 +5,8 @@ export class Enemy extends MovableObject {
         super().loadImage("../Assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
 
         this.position_x = 200 + Math.random() * 500;
-        this.position_y = 350;
-        this.width = 75;
+        this.position_y = 380;
+        this.width = 50;
         this.height = 50;
     }
 }
