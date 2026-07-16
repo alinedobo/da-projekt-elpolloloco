fbc97a7a5924d20b484310532e832d3cdc267ba5
Aline Dobo <dobo.aline@gmail.com>
2026-07-16 19:55:12 +0200

    Refactor: split up the animate method of Pepe

--- Scripts/Classes/character.class.js
+++ Scripts/Classes/character.class.js
@@ -7,13 +7,13 @@ import { MovableObject } from "./movable-object.class.js";
 import { World } from "./world.class.js";
 
 export class Character extends MovableObject {
+    //#region Properties
     world;
     position_x = 20;
     position_y = 240;
     height = 200;
     width = 100;
     speed_X = 5;
-
     offset = {
         top: 100,
         right: 30,
@@ -22,11 +22,10 @@ export class Character extends MovableObject {
     };
     showFrame = false;
     energy = 100;
-
     hitSound = SoundHub.PEPE_DAMAGE;
     dyingSound = SoundHub.PEPE_DEAD;
-
     lastMovement;
+    //#endregion
 
     constructor() {
         super().loadImage(ImageHub.PEPE.walking[0]);
@@ -37,72 +36,70 @@ export class Character extends MovableObject {
         this.loadImages(ImageHub.PEPE.idle);
         this.loadImages(ImageHub.PEPE.sleeping);
 
+        IntervalHub.startInterval(this.moveCharacter, 1000 / 60);
         IntervalHub.startInterval(this.applyGravity, 1000 / 25);
-        IntervalHub.startInterval(this.playSound, 1000/25);
-        this.animate();
+        IntervalHub.startInterval(this.playSound, 1000 / 25);
+        IntervalHub.startInterval(this.animateCharacter, 100);
         this.getRealFrame();
     }
 
-    animate() {
-        IntervalHub.startInterval(() => {
-            if (
-                Keyboard.KEY_RIGHT &&
-                this.position_x < this.world.level.level_end_x
-            ) {
-                // level_end_x is part of the level, the level is part of the world
-                // for the character to access the value level_end_x in level, we need to go up into the world and back down into the world
-                this.moveRight();
-                this.reverseDirection = false;
-            }
-
-            if (Keyboard.KEY_LEFT && this.position_x > 0) {
-                this.moveLeft();
-                this.reverseDirection = true;
-            }
-
-            if (
-                (Keyboard.KEY_UP || Keyboard.KEY_SPACE) &&
-                !this.isAboveGround()
-            ) {
-                this.jump();
-                SoundHub.playOne(SoundHub.PEPE_JUMP);
-            }
-            this.world.camera_x = -this.position_x + 100;
-        }, 1000 / 60);
-
-        IntervalHub.startInterval(() => {
-            if (this.isAboveGround()) {
-                this.playAnimation(ImageHub.PEPE.jumping);
-            } else if (this.isHurt()) {
-                this.playAnimation(ImageHub.PEPE.hurt);
-            } else if (this.isDead()) {
-                this.playAnimation(ImageHub.PEPE.dead);
-                this.speed_X = 0;
-            } else if (Keyboard.KEY_RIGHT || Keyboard.KEY_LEFT) {
-                this.playAnimation(ImageHub.PEPE.walking);
-            } else if (this.checkIfSleeping()) {
-                this.playAnimation(ImageHub.PEPE.sleeping);
-            } else {
-                this.playAnimation(ImageHub.PEPE.idle);
-            }
-        }, 100);
+    //#region Methods
+    animateCharacter = () => {
+        if (this.isAboveGround()) {
+            this.playAnimation(ImageHub.PEPE.jumping);
+        } else if (this.isHurt()) {
+            this.playAnimation(ImageHub.PEPE.hurt);
+        } else if (this.isDead()) {
+            this.playAnimation(ImageHub.PEPE.dead);
+            this.speed_X = 0;
+        } else if (Keyboard.KEY_RIGHT || Keyboard.KEY_LEFT) {
+            this.playAnimation(ImageHub.PEPE.walking);
+        } else if (this.checkIfSleeping()) {
+            this.playAnimation(ImageHub.PEPE.sleeping);
+        } else {
+            this.playAnimation(ImageHub.PEPE.idle);
+        }
     }
 
     playSound = () => {
         if (this.checkIfSleeping()) {
             SoundHub.playOneContinuously(SoundHub.PEPE_SNORING);
         }
-        
+
         if (!this.checkIfSleeping()) {
             SoundHub.pauseOne(SoundHub.PEPE_SNORING);
         }
-        
+
         if (Keyboard.KEY_RIGHT || Keyboard.KEY_LEFT) {
             SoundHub.playOneContinuously(SoundHub.PEPE_RUN);
         }
-        
+
         if (!Keyboard.KEY_RIGHT && !Keyboard.KEY_LEFT) {
             SoundHub.pauseOne(SoundHub.PEPE_RUN);
         }
     };
+
+    moveCharacter = () => {
+        if (
+            Keyboard.KEY_RIGHT &&
+            this.position_x < this.world.level.level_end_x
+        ) {
+            // level_end_x is part of the level, the level is part of the world
+            // for the character to access the value level_end_x in level, we need to go up into the world and back down into the world
+            this.moveRight();
+            this.reverseDirection = false;
+        }
+
+        if (Keyboard.KEY_LEFT && this.position_x > 0) {
+            this.moveLeft();
+            this.reverseDirection = true;
+        }
+
+        if ((Keyboard.KEY_UP || Keyboard.KEY_SPACE) && !this.isAboveGround()) {
+            this.jump();
+            SoundHub.playOne(SoundHub.PEPE_JUMP);
+        }
+        this.world.camera_x = -this.position_x + 100;
+    };
+    //#endregion
 }
