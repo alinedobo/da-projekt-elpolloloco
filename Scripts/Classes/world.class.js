ef293a80adde6dafb4374a070f32c51e3cbe4525
Aline Dobo <dobo.aline@gmail.com>
2026-07-16 16:50:05 +0200

    Fix: Increase interval frequency for collision with enemy for more accurate colliding and reduced damage pro collision

--- Scripts/Classes/world.class.js
+++ Scripts/Classes/world.class.js
@@ -130,10 +130,10 @@ export class World {
     }
 
     checkCollisions() {
-        IntervalHub.startInterval(this.checkCollisionWithBabyEnemy, 50);
-        IntervalHub.startInterval(this.checkCollisionWithEnemy, 50);
-        IntervalHub.startInterval(this.checkCollisionWithEndboss, 50);
-        IntervalHub.startInterval(this.checkCollisionWithBottle, 50);
+        IntervalHub.startInterval(this.checkCollisionWithBabyEnemy, 10);
+        IntervalHub.startInterval(this.checkCollisionWithEnemy, 10);
+        IntervalHub.startInterval(this.checkCollisionWithEndboss, 10);
+        IntervalHub.startInterval(this.checkCollisionWithBottle, 10);
         IntervalHub.startInterval(this.checkCollisionWithCoin, 10);
         IntervalHub.startInterval(this.checkBottleCollisionWithEnemies, 10);
     }
@@ -144,7 +144,7 @@ export class World {
                 enemy.energy = 0;
                 SoundHub.playOne(enemy.dyingSound);
             } else if (this.character.isColliding(enemy)) {
-                this.character.isHit(1);
+                this.character.isHit(0.25);
                 SoundHub.playOne(this.character.hitSound);
                 this.healthBar.showPercentageStatusBar(this.character.energy);
             }
@@ -157,7 +157,7 @@ export class World {
                 enemy.energy = 0;
                 SoundHub.playOne(enemy.dyingSound);
             } else if (this.character.isColliding(enemy)) {
-                this.character.isHit(2);
+                this.character.isHit(0.5);
                 this.healthBar.showPercentageStatusBar(this.character.energy);
             }
         });
@@ -166,7 +166,7 @@ export class World {
     checkCollisionWithEndboss = () => {
         this.level.endBosses.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
-                this.character.isHit(5);
+                this.character.isHit(0.75);
                 this.healthBar.showPercentageStatusBar(this.character.energy);
             }
         });
